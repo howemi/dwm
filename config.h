74d858ddfdbf0279e3e60430fdd74768d030c4b1
Michael Howe <mike@olympiate.com>
2020-04-19 02:25:06 -0600

    Update colors

--- config.h
+++ config.h
@@ -12,15 +12,19 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_normbg[]      = "#222222";
+static const char col_normborder[]  = "#444444";
+static const char col_normfg[]      = "#bbbbbb";
+static const char col_selfg[]       = "#eeeeee";
+static const char col_selbg[]       = "#db7a27";
+/* static const char col_cyan[]     = "#005577"; */
+static const char col_selborder[]   = "#db7a27";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/* [SchemeNorm] = { col_gray3, col_gray1, col_gray2 }, */
+	/* [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }, */
+	[SchemeNorm] = { col_normfg, col_normbg, col_normborder },
+	[SchemeSel]  = { col_selfg, col_selbg,  col_selborder  },
 };
 
 /* tagging */
@@ -72,7 +76,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_normbg, "-nf", col_normfg, "-sb", col_selborder, "-sf", col_selfg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
