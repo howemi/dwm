9bc1771c707d980c7cd43460f850d48a674d79fc
Michael Howe <mike@olympiate.com>
2020-08-18 17:15:31 -0600

    update keybinds, remove border pixels

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -149,6 +149,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
 	{ MODKEY,             		    XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY,                 XK_bracketright, spawn,	   SHCMD("setaudio") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -173,7 +174,6 @@ static Key keys[] = {
 	{ 0, XF86XK_AudioRaiseVolume,  	spawn,	   SHCMD("amixer -q sset Master 5%+") },
 	{ 0, XF86XK_MonBrightnessUp,  	spawn,	   SHCMD("sudo brightnessctl -d 'intel_backlight' set 5%+") },
 	{ 0, XF86XK_MonBrightnessDown, 	spawn,	   SHCMD("sudo brightnessctl -d 'intel_backlight' set 5%-") },
-	{ 0, XF86XK_Eject,  		    spawn,	   SHCMD("setaudio") },
 };
 
 /* button definitions */
